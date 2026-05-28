3bfa0b4c33216f8eed067704101e246633eca963
suraj8957 <suraj.tripathi.snaatak@mygurukulam.co>
2026-05-29 03:54:38 +0530

    Update main.tf

--- env/dev/ec2/main.tf
+++ env/dev/ec2/main.tf
@@ -1,8 +1,8 @@
 module "ec2" {
   source = "git::https://github.com/suraj8957/terraform-modules.git//ec2"
 
-  region        = var.dev-region
-  ami           = var.dev-instance-ami
-  instance-type = var.dev-instance-type
-  instance-name = var.dev-instance-name
+  region        = var.dev_region
+  ami           = var.dev_instance_ami
+  instance-type = var.dev_instance_type
+  instance-name = var.dev_instance_name
 }
