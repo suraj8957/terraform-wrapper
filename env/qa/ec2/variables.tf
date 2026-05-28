e7398019377b5bb142066d2952ed9b7ba9d74bd6
suraj8957 <suraj.tripathi.snaatak@mygurukulam.co>
2026-05-29 03:15:51 +0530

    Create variables.tf

---
+++ env/qa/ec2/variables.tf
@@ -0,0 +1,16 @@
+variable "qa-region" {
+  type        = string
+  description = "dev region"
+}
+variable "qa-instance-ami" {
+  type        = string
+  description = "dev instance ami id"
+}
+variable "qa-instance-type" {
+  type        = string
+  description = "dev instance type"
+}
+variable "qa-instance-name" {
+  type        = string
+  description = "name of the dev instance"
+}
